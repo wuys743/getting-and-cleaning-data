f6d460c602cf48a477697e44457314d1d5980786
wuys743 <83258648+wuys743@users.noreply.github.com>
2021-06-29 17:53:46 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,78 +1,61 @@
-## Merges the train and test dataset
-merge_train_test_dataset <- function(directory = "UCI HAR Dataset"){
-  # params:
-  # directory: path where train and test directories are stored
-  # return: 
-  # Merged train and test dataset including subject and activity (label)
-  # with descriptive variable names.
-  
-  # Variable names/feature names
-  features <- read.table(paste("./", "UCI HAR Dataset", "/features.txt", sep = ""))
-  variable_names <- c("subject", as.character(features[, 2]), "activity")
-  
-  # Train set
-  subject_train <- read.table(paste("./", directory, "/train/subject_train.txt", sep = ""))
-  train_set_X <- read.table(paste("./", directory, "/train/X_train.txt", sep = ""))
-  train_set_Y <- read.table(paste("./", directory, "/train/y_train.txt", sep = ""))
-  train_set <- cbind(subject_train, train_set_X, train_set_Y)
-  
-  # Test set
-  subject_test <- read.table(paste("./", directory, "/test/subject_test.txt", sep = ""))
-  test_set_X <- read.table(paste("./", directory, "/test/X_test.txt", sep = "" ))
-  test_set_Y <- read.table(paste("./", directory, "/test/y_test.txt", sep = "" ))
-  test_set <- cbind(subject_test, test_set_X, test_set_Y)
-  
-  complete_dataset <- rbind(train_set, test_set)
-  
-  names(complete_dataset) <- variable_names
-  
-  return(complete_dataset)
-}
+## 1. Merge the training and the test sets to create one data set.
 
-# Transform the activities into Descriptive activities
+##step 1: download zip file from website
+if(!file.exists("./data"))dir.create("./data")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile = "./data/projectData_getCleanData.zip")
 
-activities_transforming_into_descriptive_activities <- function(directory = "UCI HAR Dataset"){
-  # params:
-  # directory: path containing train and test directories
-  # return:
-  # A merged train and test dataset with descriptive variable names as well as descriptive
-  # activities name
-  complete_dataset <- merge_train_test_dataset(directory)
-  activities <- read.table(paste("./", directory, "/activity_labels.txt", sep = ""))
-  activities[, 2] <- as.character(activities[, 2])
-  for(i in 1:6){
-    complete_dataset$activity <- gsub(i, activities[, 2][i], complete_dataset$activity)
-  }
-  
-  return(complete_dataset)
-  
-}
+## step 2: unzip data
+listZip <- unzip("./data/projectData_getCleanData.zip", exdir = "./data")
 
-# Extracts only the measurements on the mean and standard deviation for each measurement.
-extract_measurements_mean_std <- function(directory = "UCI HAR Dataset"){
-  # params:
-  # directory: path containing train and test directories
-  # return:
-  # A Dataframe containing the measurements on the mean and 
-  # standard deviation for each measurement.
-  complete_dataset <- activities_transforming_into_descriptive_activities(directory)
-  indexes_mean_std <- grep('mean|std', names(complete_dataset))
-  measurements_mean_std <- complete_dataset[, c(1, indexes_mean_std, 563)]
-  return(measurements_mean_std)
-}
+## step 3: load data into R
+train.x <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+train.y <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+train.subject <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+test.x <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+test.y <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+test.subject <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 
-average_activity_subject <- function(directory = "UCI HAR Dataset"){
-  # params:
-  # directory: path containing train and test directories
-  # return:
-  # A Dataframe containing average of each variable for each activity and each subject.
-  require(dplyr)
-  complete_df <- extract_measurements_mean_std(directory)
-  summarized <- complete_df %>%
-    group_by(activity, subject) %>%
-    summarise_at(vars(names(complete_df)[2]: names(complete_df)[80]), mean)
-}
+## step 4: merge train and test data
+trainData <- cbind(train.subject, train.y, train.x)
+testData <- cbind(test.subject, test.y, test.x)
+fullData <- rbind(trainData, testData)
 
 
-summary <- average_activity_subject()
-write.table(summary, './tidy_dataset.txt', row.names=FALSE)
+## 2. Extract only the measurements on the mean and standard deviation for each measurement. 
+
+## step 1: load feature name into R
+featureName <- read.table("./data/UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)[,2]
+
+## step 2: extract mean and standard deviation of each measurements
+featureIndex <- grep(("mean\\(\\)|std\\(\\)"), featureName)
+finalData <- fullData[, c(1, 2, featureIndex+2)]
+colnames(finalData) <- c("subject", "activity", featureName[featureIndex])
+
+
+## 3. Uses descriptive activity names to name the activities in the data set
+
+## step 1: load activity data into R
+activityName <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+
+## step 2: replace 1 to 6 with activity names
+finalData$activity <- factor(finalData$activity, levels = activityName[,1], labels = activityName[,2])
+
+
+## 4. Appropriately labels the data set with descriptive variable names.
+
+names(finalData) <- gsub("\\()", "", names(finalData))
+names(finalData) <- gsub("^t", "time", names(finalData))
+names(finalData) <- gsub("^f", "frequence", names(finalData))
+names(finalData) <- gsub("-mean", "Mean", names(finalData))
+names(finalData) <- gsub("-std", "Std", names(finalData))
+
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+library(dplyr)
+groupData <- finalData %>%
+  group_by(subject, activity) %>%
+  summarise_each(funs(mean))
+
+write.table(groupData, "./Getting_and_Cleaning_data_Project/MeanData.txt", row.names = FALSE)
+
